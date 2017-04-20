96d8d087a4a0d7872ec0139c3bf9c40420069cba
Thierry Strudel <tstrudel@google.com>
2017-04-19 22:43:02 -0700

    device: don't use generic GK and KM HALs
    Change-Id: I2f862dcc0639a96d1cb0f4c613d0d18d4847ca95
    (cherry picked from commit cb0d612923e690e75d2a78405c632b0f9250cde5)

--- device.mk
+++ device.mk
@@ -241,15 +241,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     esed
 
-# gatekeeper HAL
-PRODUCT_PACKAGES +=                         \
-    android.hardware.gatekeeper@1.0-impl    \
-    android.hardware.gatekeeper@1.0-service
-
-# Keymaster HAL
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
-
 # Memtrack HAL
 PRODUCT_PACKAGES += \
     memtrack.msm8998 \
