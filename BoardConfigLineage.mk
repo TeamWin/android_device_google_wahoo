8914fe0d21d80870c63d656fe4f86d2371df7110
Christopher N. Hesse <raymanfx@gmail.com>
2017-11-25 20:14:34 +0100

    wahoo: Build device tree overlay image
    Change-Id: Ia81db83b24286cbd2b4aaacb09a085d304f5c50a

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -5,5 +5,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := lineageos_wahoo_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/wahoo
+TARGET_NEEDS_DTBOIMAGE := true
 
 -include vendor/google/wahoo/BoardConfigVendor.mk
