d5ad5ed823d403aeae8f67fc6a08263b3c48f2eb
Christopher N. Hesse <raymanfx@gmail.com>
2018-01-11 11:11:20 +0100

    wahoo: Remove defconfig flag
    This is moved into muskie/taimen trees.
    
    Change-Id: I42edf5339d13a7393db4fa28dd165a3f64ea590f

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,7 +3,6 @@
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_CONFIG := lineageos_wahoo_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/wahoo
 TARGET_NEEDS_DTBOIMAGE := true
 
