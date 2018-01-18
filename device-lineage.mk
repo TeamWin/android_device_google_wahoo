4c4c7582efe49c5985092914619ee5054024b011
Abhisek Devkota <ciwrl@lineageos.org>
2018-01-17 21:53:21 -0800

    IMS Support
    Build what we can
    
    Change-Id: I9e482b9c35131e3634aaf7a6c8ba05ade11b28fb

--- device-lineage.mk
+++ device-lineage.mk
@@ -4,3 +4,9 @@ DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-lineage
 # EUICC feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# IMS
+PRODUCT_PACKAGES += \
+   com.android.ims.rcsmanager \
+   RcsService \
+   PresencePolling
